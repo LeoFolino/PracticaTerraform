44bc6569f469bc4f4e8f7a91865cf1dd222dc5b4
LeoFolino <leonelfolinoing@gmail.com>
2024-07-26 00:44:35 -0300

    Se aniade el proceso de modificacion y ejecucion de archivo tf

--- MiPracticaTerraform/Practica1/terraform.tf
+++ MiPracticaTerraform/Practica1/terraform.tf
@@ -1,10 +1,10 @@
 # No hay nombres reservados para nombrar archivos en Terraform
 
-resource "local_file" "productos" {
+# resource "local_file" "productos" {
 
-    content = "Lista de productos"
-    filename = "productos.txt"
-}
+#     content = "Lista de productos"
+#     filename = "productos.txt"
+# }
 
 # Para ejecutar el codigo (En Windows) se usa: 
 # terraform init                                  (Este comando va a descargar el provider necesario para deployar el codigo. Esto lo determina por el tipo de bloque de codigo que tenemos)
@@ -14,3 +14,14 @@ resource "local_file" "productos" {
 
 # para llevar a cabo el deployment se ejecuta
 # terraform apply                               # este apply de fondo crea un plan y lo vuelve a presentar, NO SE BASA LO PRESENTADO CON EL COMANDO "terraform plan", al indicar yes avanza con la ejecucion
+
+
+# Si quisiera hacer un cambio, se debe realizar en el codigo, de la siguiente manera
+
+resource "local_file" "productos" {
+
+    content = "Lista de productos para el mes proximo"
+    filename = "productos.txt"
+}
+
+# Luego hacemos un terraform plan, vamos a ver que nos avisa que va a destruir el archivo y lo va a volver a crear, ya que no puede cambiar o editar el contenido del archivo mismo
