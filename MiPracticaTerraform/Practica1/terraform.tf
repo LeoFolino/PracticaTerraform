48ac4553c096f0da2ad1076c5855dbf7fbbf29a3
LeoFolino <leonelfolinoing@gmail.com>
2024-07-26 01:41:30 -0300

    Se corrige falta de ortografía y texto

--- MiPracticaTerraform/Practica1/terraform.tf
+++ MiPracticaTerraform/Practica1/terraform.tf
@@ -9,7 +9,7 @@
 # Para ejecutar el codigo (En Windows) se usa: 
 # terraform init                                  (Este comando va a descargar el provider necesario para deployar el codigo. Esto lo determina por el tipo de bloque de codigo que tenemos)
 # terraform plan                                  (Este comando va a generar un plan en base a nuestro codigo y va a presentarlo indicando las acciones que va a realizar)
-                                               # Indica que ki qye va a crear se visualiza con un + en verde y va a realizar la lista de acciones descriptas
+                                               # Indica que lo que va a crear se visualiza con un + en verde y va a realizar la lista de acciones descriptas
                                                # Si bien utilizamos content y filename, hay mas parametros. 
 
 # para llevar a cabo el deployment se ejecuta
