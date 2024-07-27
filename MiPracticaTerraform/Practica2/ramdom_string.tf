cbc8b72c742ffc8f85a083acf370585d93882324
LeoFolino <leonelfolinoing@gmail.com>
2024-07-27 16:41:26 -0300

    Se crean mas sufijos y se aniade explicacion

--- MiPracticaTerraform/Practica2/ramdom_string.tf
+++ MiPracticaTerraform/Practica2/ramdom_string.tf
@@ -1,6 +1,36 @@
-resource "random_string" "sufijo" {
+resource "random_string" "sufijo-1" {
   length           = 10
   special          = true
   override_special = "/@£$#%&"
   upper = true
 }
+
+resource "random_string" "sufijo-2" {
+  length           = 10
+  special          = true
+  override_special = "/@£$#%&"
+  upper = true
+}
+
+resource "random_string" "sufijo-3" {
+  length           = 10
+  special          = true
+  override_special = "/@£$#%&"
+  upper = true
+}
+
+resource "random_string" "sufijo-4" {
+  length           = 10
+  special          = true
+  override_special = "/@£$#%&"
+  upper = true
+}
+
+resource "random_string" "sufijo-5" {
+  length           = 10
+  special          = true
+  override_special = "/@£$#%&"
+  upper = true
+}
+
+# Se cambia el nombre del recurso al igual que en local_file.tf. Ya que no puedo tener el mismo nombre de recurso para reiterados recursos.
