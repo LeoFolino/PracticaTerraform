8276e02bf4cd17ef3f6295091f00f46c0570fd13
LeoFolino <leonelfolinoing@gmail.com>
2024-07-28 20:20:02 -0300

    Se empieza a trabajar con versiones establecidas

---
+++ MiPracticaTerraform/Practica4/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.56.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# como estamos apuntando a la version de aws bastante anterior, ahora no va a bajar la ultima, sino la que le estamos pidiendo
+# /capturas/terraform-init-provider-v
+
