198768efba5805fe20dc80ee03798108ea75bb6b
LeoFolino <leonelfolinoing@gmail.com>
2024-08-05 20:58:51 -0300

    Adicion de tags

--- MiPracticaTerraform/Practica6-1/vpc.tf
+++ MiPracticaTerraform/Practica6-1/vpc.tf
@@ -10,9 +10,17 @@ resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc_virginia.id
   cidr_block = var.subnet[0]
   map_public_ip_on_launch = true
+  tags = {
+    Name = "Public Subnet"
+    env = "Dev"
+  }
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.vpc_virginia.id
   cidr_block = var.subnet[1]
+  tags = {
+    Name = "Private Subnet"
+    env = "Dev"
+  }
 }
