fc01c2910dc7211c5c1b193f34ed85ca717d60cf
LeoFolino <leonelfolinoing@gmail.com>
2024-08-05 20:49:09 -0300

    adicion config vpc privada y publica

--- MiPracticaTerraform/Practica6/vpc.tf
+++ MiPracticaTerraform/Practica6/vpc.tf
@@ -8,4 +8,11 @@ resource "aws_vpc" "vpc_virginia" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc_virginia.id
+  cidr_block = var.public_subnet
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.vpc_virginia.id
+  cidr_block = var.public_subnet
 }
